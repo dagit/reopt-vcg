0d83d51b97bd98b99655e63582cf4898f4ac9814
Jason Dagit <dagitj@gmail.com>
2018-11-09 15:38:50 -0800

    Add support for a few bitwise operations

--- lean/bitvec.lean
+++ lean/bitvec.lean
@@ -60,6 +60,13 @@ lemma in_range (x n : ℕ) : x % 2 ^ n < 2 ^ n :=
     simp [zero_lt_succ]
   end
 
+lemma div_2_lt {x n:ℕ} (H: x < 2^(n+1)) : x / 2 < 2^n :=
+  begin
+    rw [pow_succ, ←div_lt_iff_lt_mul] at H,
+    exact H,
+    apply succ_pos
+  end
+
 section shift
   variable {n : ℕ}
 
@@ -69,7 +76,10 @@ section shift
   def lsb (x: bitvec n) : bool := x.val % 2 = 1
 
   -- unsigned shift right
-  def ushr (x : bitvec n) (i : ℕ) : bitvec n := ⟨x.val / 2^i % 2^n, by simp [in_range]⟩
+  def ushr (x : bitvec n) (i : ℕ) : bitvec n := ⟨x.val / 2^i,
+    calc
+        x.val / 2^i ≤ x.val : by apply nat.div_le_self
+                ... < 2^n   : by exact x.property⟩
 
   -- signed shift right
   def sshr {H: n > 0} (x: bitvec n) (i:ℕ) : bitvec n :=
@@ -86,13 +96,144 @@ section shift
 
 end shift
 
+def sub_less {a x y : ℕ} (H: x < y) : x - a < y :=
+  calc
+    x - a ≤ x : sub_le x a
+      ... < y : H
+
+lemma mul_lt_self {m k:ℕ} (Hk: k > 0) (Hm: m > 1): k < k * m :=
+  begin
+    have h: 1 < m,
+    { exact Hm },
+    have ans: k * 1 < k * m,
+    { apply (nat.mul_lt_mul_of_pos_left h Hk) },
+    rw mul_one at ans,
+    exact ans
+  end
+
+@[reducible]
+def bit_to_nat : bool → ℕ
+  | tt := 1
+  | ff := 0
+
+lemma bounded_bit_to_nat {b:bool} : bit_to_nat b ≤ 1 := by cases b; { unfold bit_to_nat, try {apply zero_le} }
+
+lemma sub_right {n m k: ℕ} (H: n + k = m) : n = m - k := by rw [←H, nat.add_sub_cancel]
+
+lemma pow_2_sum {n:ℕ} : 2^(n+1) = 2^n + 2^n :=
+  calc
+    2^(n+1) = 2^n * 2           : by rw pow_succ
+        ... = 2^n * (1 + 1)     : by refl
+        ... = 2^n * 1 + 2^n * 1 : by rw nat.left_distrib
+        ... = 2^n + 2^n         : by simp
+
+lemma product_eq {n: ℕ} (y:ℕ) (xs : list bool) (len: list.length xs = n) : list.foldl (λacc _, acc*2 + 1) y xs = 2^n * y + 2^n - 1 :=
+  begin
+    induction xs generalizing n y,
+    { simp, simp at len, rw ←len, simp },
+    { have len_tl := sub_right len,
+      have ih_val := @xs_ih (n-1) (1 + y * 2) len_tl,
+      simp, simp at ih_val,
+      rw left_distrib (2^(n-1)) at ih_val,
+      simp at ih_val, rw ←nat.add_assoc (2^(n-1)) at ih_val,
+      have n_pos:  n > 0, { rw [list.length_cons] at len, rw ←len, apply zero_lt_succ},
+      have cancel: n - 1 + 1 = n, { apply (succ_pred_eq_of_pos n_pos) },
+      have cancel2: succ (n - 1) = n, { apply (succ_pred_eq_of_pos n_pos) },
+      rw [eq.symm pow_2_sum, cancel, nat.mul_comm _ 2, ←nat.mul_assoc (2^(n-1)) 2 y, ←pow_succ, cancel2] at ih_val,
+      rw nat.mul_comm _ 2, exact ih_val
+    }
+  end
+
+lemma foldr_append {α β: Type} (f: α → β → β) (y: β) (xs ys: list α) : list.foldr f y (xs ++ ys) = list.foldr f (list.foldr f y ys) xs :=
+  begin
+    induction xs with xs,
+    { refl },
+    { simp, rw xs_ih }
+  end
+
+lemma reverse_core_cons {α: Type} (ys xs: list α) : list.reverse_core xs ys = list.reverse_core xs list.nil ++ ys :=
+  begin
+    induction xs with xs generalizing ys,
+    { refl },
+    { simp [list.reverse_core], rw xs_ih [xs], simp, rw ←xs_ih}
+  end
+
+lemma reverse_cons {α: Type} (x: α) (xs: list α) : list.reverse (x :: xs) = list.reverse xs ++ [x] :=
+  begin
+    induction xs with xs,
+    { refl },
+    { unfold list.reverse, rw list.reverse_core, rw reverse_core_cons }
+  end
+
+lemma foldl_foldr {α β: Type} (f: α → β → α) (y: α) (xs: list β) : list.foldl f y xs = list.foldr (λa b, f b a) y (xs.reverse) :=
+  begin
+    induction xs with xs generalizing y,
+    case list.nil
+    { refl },
+    case list.cons
+    { simp, rw [reverse_cons, foldr_append, xs_ih], refl },
+  end
+
+lemma lt_q_r {b q r :ℕ} (Hb: b ≤ 1) (H: q ≤ r) : b + q*2 ≤ 1 + r * 2 :=
+  calc
+    b + q*2 ≤ 1 + q * 2 : by apply nat.add_le_add_right Hb
+        ... ≤ 1 + r * 2 : by apply nat.add_le_add_left (nat.mul_le_mul_right 2 H)
+
+lemma foldr_product_bound {y: ℕ} {xs : list bool} :
+    list.foldr (λ (a : bool) (b : ℕ), b * 2 + bit_to_nat a) y xs ≤ list.foldr (λ (a : bool) (b : ℕ), b * 2 + 1) y xs :=
+  begin
+    induction xs with b,
+    case list.nil
+    { refl },
+    case list.cons
+    { simp, apply lt_q_r, apply bounded_bit_to_nat,
+      simp at xs_ih,
+      apply xs_ih,
+    }
+  end
+
+lemma product_bound (y: ℕ) (xs : list bool) :
+                    list.foldl (λacc b, acc*2 + bit_to_nat b) y xs ≤ list.foldl (λacc _, acc*2 + 1) y xs :=
+  begin
+    rw [foldl_foldr, foldl_foldr],
+    apply foldr_product_bound
+  end
+
 section bitwise
   variable {n : ℕ}
 
-  def not : bitvec n → bitvec n := map bnot
-  def and : bitvec n → bitvec n → bitvec n := map₂ band
-  def or  : bitvec n → bitvec n → bitvec n := map₂ bor
-  def xor : bitvec n → bitvec n → bitvec n := map₂ bxor
+  def bitvec_to_bits : Π{n:ℕ} (x: bitvec n), vector bool n
+    | 0     _ := nil
+    | (n+1) x := msb x :: @bitvec_to_bits n ⟨x.val / 2, by apply (div_2_lt x.property)⟩
+
+  def bits_to_bitvec (x: vector bool n) : bitvec n :=
+    ⟨ list.foldl (λacc b, acc*2 + bit_to_nat b) 0 (to_list x),
+      begin
+        have bounded := product_bound 0 (to_list x),
+        rw [product_eq 0 (to_list x) x.property, nat.mul_comm (2^n) 0, zero_mul, zero_add] at bounded,
+        have bounded := lt_succ_of_le bounded,
+        have pos : 2^n > 0, { apply pos_pow_of_pos, apply succ_pos },
+        have succ_rw : succ (2 ^ n - 1) = succ (pred (2 ^ n)), { refl },
+        rw [succ_rw, succ_pred_eq_of_pos pos] at bounded,
+        exact bounded
+      end⟩
+
+  @[reducible]
+  def bit_map₂ (f: bool → bool → bool) (x: bitvec n) (y: bitvec n) : bitvec n :=
+    bits_to_bitvec (map₂ f (bitvec_to_bits x) (bitvec_to_bits y))
+
+  def not (x: bitvec n) : bitvec n := ⟨2^n - x.val - 1,
+    begin
+      have xval_pos : 0 < x.val + 1,
+      { apply (succ_pos x.val) },
+      apply (sub_lt _ xval_pos),
+      apply pos_pow_of_pos,
+      apply (succ_pos 1)
+    end⟩
+
+  def and : bitvec n → bitvec n → bitvec n := bit_map₂ band
+  def or  : bitvec n → bitvec n → bitvec n := bit_map₂ bor
+  def xor : bitvec n → bitvec n → bitvec n := bit_map₂ bxor
 
 end bitwise
 
