df25a61f17128bc1fda7ec3363e5623093c000c0
Jason Dagit <dagitj@gmail.com>
2018-11-13 13:58:49 -0800

    reimplement bitwise operations and provide proof of sshr with using modulo

--- lean/bitvec.lean
+++ lean/bitvec.lean
@@ -9,6 +9,8 @@ This is a work-in-progress, and contains additions to other theories.
 -/
 import data.vector
 
+import .common -- for dec_trivial_tac
+
 @[reducible] def bitvec (sz:ℕ) :=
   { x : ℕ // x < (2 ^ sz) }
 
@@ -23,30 +25,15 @@ open vector
 def zero (n : ℕ) : bitvec n :=
   ⟨0, nat.pos_pow_of_pos n dec_trivial⟩
 
--- TODO: is this in the standard library somewhere?
-def one_to_the_n (n:ℕ) : (1 ^ n) = 1 :=
-  begin
-    induction n,
-    rw nat.pow_zero,
-    rw nat.pow_succ,
-    rw n_ih,
-    refl
-  end
+instance {n:ℕ} : has_zero (bitvec n) := ⟨bitvec.zero n⟩
 
 -- Create a bitvector with the constant one.
 @[reducible] protected
 def one (n: ℕ) {H : n > 0} : bitvec n :=
-  ⟨1,
-    begin
-      intros,
-
-      have one_to_the_n_twice : (1 ^ n) + (1 ^ n) = 2,
-        rw one_to_the_n,
-
-      rw [←one_to_the_n n, bit0, one_to_the_n_twice],
-      apply (@nat.pow_lt_pow_of_lt_left 1 2 dec_trivial n H),
-
-    end⟩
+  ⟨1, calc
+        1   < 2   : by dec_trivial_tac
+        ... = 2^1 : by trivial
+        ... ≤ 2^n : by apply (pow_le_pow_of_le_right (zero_lt_succ _) (succ_le_of_lt H))⟩
 
 instance {n:ℕ} {H: n > 0} : has_one (bitvec n)  := ⟨@bitvec.one n H⟩
 
@@ -60,29 +47,97 @@ lemma in_range (x n : ℕ) : x % 2 ^ n < 2 ^ n :=
     simp [zero_lt_succ]
   end
 
-lemma div_2_lt {x n:ℕ} (H: x < 2^(n+1)) : x / 2 < 2^n :=
+lemma div_lt_of_lt_mul {m n : ℕ} : ∀ {k} {Hk : k > 0}, m < k * n → m / k < n
+  | 0        Hk h := by apply absurd Hk (lt_irrefl 0)
+  | (succ k) Hk h :=
+    suffices succ k * (m / succ k) < succ k * n, from lt_of_mul_lt_mul_left this (zero_le _),
+    calc
+      succ k * (m / succ k) ≤ m % succ k + succ k * (m / succ k) : le_add_left _ _
+                        ... = m                                  : by rw mod_add_div
+                        ... < succ k * n                         : h
+
+lemma pow_mul {b n m :ℕ} : b^n * b^m = b^(n+m) :=
   begin
-    rw [pow_succ, ←div_lt_iff_lt_mul] at H,
-    exact H,
-    apply succ_pos
+    induction m with m,
+    case nat.zero
+    { simp },
+    case nat.succ
+    { rw [pow_succ, ←mul_assoc (b^n), m_ih, ←pow_succ] }
+  end
+
+lemma sub_lt_left {a b} (c:ℕ) (H: a < b) (Ha: a > c) : a - c < b - c :=
+  begin
+    have Hb : b > c, { apply lt_trans Ha H },
+    induction c with c generalizing a b,
+    case nat.zero
+    { rwa nat.sub_zero },
+    case nat.succ
+    { have a_gt : a > c, { apply lt_of_succ_lt Ha},
+      have b_gt : b > c, { apply lt_of_succ_lt Hb},
+      cases a,
+      { simp at Ha,
+        have : false, { apply not_lt_zero _ a_gt },
+        contradiction,
+      },
+      cases b,
+      { simp at Hb,
+        have : false, { apply not_lt_zero _ b_gt },
+        contradiction,
+      },
+      simp,
+      apply c_ih (lt_of_succ_lt_succ H) (lt_of_succ_lt_succ Ha) (lt_of_succ_lt_succ Hb),
+    }
+  end
+
+lemma sub_add_big_med_small (a b c : ℕ) (Hab: a > b) (Hbc : b > c) : a - b + c < a :=
+  begin
+    induction c with c generalizing a b,
+    case nat.zero
+    { apply sub_lt_of_pos_le _ _ Hbc (le_of_lt Hab) },
+    { cases b,
+      case nat.zero
+      { have : false, { apply not_lt_zero _ Hbc},
+        contradiction,
+      },
+      { cases a,
+        case nat.zero
+        { have : false, { apply not_lt_zero _ Hab },
+          contradiction,
+        },
+        case nat.succ
+        { have Hbc: b > c, { apply lt_of_succ_lt_succ Hbc },
+          have Hab: a > b, { apply lt_of_succ_lt_succ Hab },
+          have := c_ih _ _ Hab Hbc,
+          simp,
+          rw [nat.add_comm],
+          apply succ_lt_succ this,
+        }
+      },
+    },
   end
 
 section shift
   variable {n : ℕ}
 
+  @[reducible]
   def shl (x : bitvec n) (i : ℕ) : bitvec n := ⟨x.val * 2^i % 2^n, by simp [in_range]⟩
 
+  @[reducible]
   def msb (x: bitvec n) : bool := (shl x (n-1)).val = 1
+
+  @[reducible]
   def lsb (x: bitvec n) : bool := x.val % 2 = 1
 
   -- unsigned shift right
+  @[reducible]
   def ushr (x : bitvec n) (i : ℕ) : bitvec n := ⟨x.val / 2^i,
     calc
         x.val / 2^i ≤ x.val : by apply nat.div_le_self
                 ... < 2^n   : by exact x.property⟩
 
   -- signed shift right
-  def sshr {H: n > 0} (x: bitvec n) (i:ℕ) : bitvec n :=
+  @[reducible]
+  def sshr {Hn: n > 0} (x: bitvec n) (i:ℕ) {Hi_lt_n: i ≤ n} {Hzero_lt_0 : 0 < i} : bitvec n :=
     -- When the sign bit is set in x, (msb x = 1), then we would like
     -- the result of sshr x i, to have the top i bits set.
     -- We can calculate a number that will do this in steps:
@@ -90,139 +145,79 @@ section shift
     -- 2) (2 ^ (n-i)) - 1, will be a number with the lower (n-i) bits set.
     -- 3) subtract the previous two numbers (1) - (2), to get a
     -- number where only the top i bits are set.
-    let upper_bits := 2 ^ n - 1 - (2 ^ (n-i) - 1) in
-    ⟨ ((ushr x i).val + if (msb x) then upper_bits else 0) % 2 ^ n,
-      by cases (msb x); { simp [upper_bits], apply in_range } ⟩
+    let upper_bits := 2 ^ n - 2 ^ (n-i) in
+    ⟨ ((ushr x i).val + if (msb x) then upper_bits else 0),
+      begin
+        cases (msb x),
+        case bool.ff
+        { simp [ushr],
+          calc
+            x.val / 2^i ≤ x.val : by apply nat.div_le_self
+                    ... < 2^n   : x.property
+        },
+        case bool.tt
+        { simp [upper_bits, ushr],
+          have : x.val/2^i < 2^(n-i),
+          { apply div_lt_of_lt_mul,
+            apply pos_pow_of_pos,
+            dec_trivial_tac,
+            rw [pow_mul, add_sub_of_le],
+            apply x.property,
+            apply Hi_lt_n,
+          },
+          apply sub_add_big_med_small (2^n) (2^(n-i)) (x.val/2^i) _ this,
+          have : 2^n > 2^(n-i),
+          { apply pow_lt_pow_of_lt_right (lt_succ_self _)
+                                         (sub_lt_of_pos_le _ _ Hzero_lt_0 Hi_lt_n)
+          },
+          assumption
+        }
+      end⟩
 
 end shift
 
-def sub_less {a x y : ℕ} (H: x < y) : x - a < y :=
-  calc
-    x - a ≤ x : sub_le x a
-      ... < y : H
-
-lemma mul_lt_self {m k:ℕ} (Hk: k > 0) (Hm: m > 1): k < k * m :=
-  begin
-    have h: 1 < m,
-    { exact Hm },
-    have ans: k * 1 < k * m,
-    { apply (nat.mul_lt_mul_of_pos_left h Hk) },
-    rw mul_one at ans,
-    exact ans
-  end
-
-@[reducible]
-def bit_to_nat : bool → ℕ
-  | tt := 1
-  | ff := 0
-
-lemma bounded_bit_to_nat {b:bool} : bit_to_nat b ≤ 1 := by cases b; { unfold bit_to_nat, try {apply zero_le} }
-
-lemma sub_right {n m k: ℕ} (H: n + k = m) : n = m - k := by rw [←H, nat.add_sub_cancel]
-
-lemma pow_2_sum {n:ℕ} : 2^(n+1) = 2^n + 2^n :=
-  calc
-    2^(n+1) = 2^n * 2           : by rw pow_succ
-        ... = 2^n * (1 + 1)     : by refl
-        ... = 2^n * 1 + 2^n * 1 : by rw nat.left_distrib
-        ... = 2^n + 2^n         : by simp
-
-lemma product_eq {n: ℕ} (y:ℕ) (xs : list bool) (len: list.length xs = n) : list.foldl (λacc _, acc*2 + 1) y xs = 2^n * y + 2^n - 1 :=
-  begin
-    induction xs generalizing n y,
-    { simp, simp at len, rw ←len, simp },
-    { have len_tl := sub_right len,
-      have ih_val := @xs_ih (n-1) (1 + y * 2) len_tl,
-      simp, simp at ih_val,
-      rw left_distrib (2^(n-1)) at ih_val,
-      simp at ih_val, rw ←nat.add_assoc (2^(n-1)) at ih_val,
-      have n_pos:  n > 0, { rw [list.length_cons] at len, rw ←len, apply zero_lt_succ},
-      have cancel: n - 1 + 1 = n, { apply (succ_pred_eq_of_pos n_pos) },
-      have cancel2: succ (n - 1) = n, { apply (succ_pred_eq_of_pos n_pos) },
-      rw [eq.symm pow_2_sum, cancel, nat.mul_comm _ 2, ←nat.mul_assoc (2^(n-1)) 2 y, ←pow_succ, cancel2] at ih_val,
-      rw nat.mul_comm _ 2, exact ih_val
-    }
-  end
-
-lemma foldr_append {α β: Type} (f: α → β → β) (y: β) (xs ys: list α) : list.foldr f y (xs ++ ys) = list.foldr f (list.foldr f y ys) xs :=
-  begin
-    induction xs with xs,
-    { refl },
-    { simp, rw xs_ih }
-  end
-
-lemma reverse_core_cons {α: Type} (ys xs: list α) : list.reverse_core xs ys = list.reverse_core xs list.nil ++ ys :=
-  begin
-    induction xs with xs generalizing ys,
-    { refl },
-    { simp [list.reverse_core], rw xs_ih [xs], simp, rw ←xs_ih}
-  end
-
-lemma reverse_cons {α: Type} (x: α) (xs: list α) : list.reverse (x :: xs) = list.reverse xs ++ [x] :=
-  begin
-    induction xs with xs,
-    { refl },
-    { unfold list.reverse, rw list.reverse_core, rw reverse_core_cons }
-  end
-
-lemma foldl_foldr {α β: Type} (f: α → β → α) (y: α) (xs: list β) : list.foldl f y xs = list.foldr (λa b, f b a) y (xs.reverse) :=
-  begin
-    induction xs with xs generalizing y,
-    case list.nil
-    { refl },
-    case list.cons
-    { simp, rw [reverse_cons, foldr_append, xs_ih], refl },
-  end
-
-lemma lt_q_r {b q r :ℕ} (Hb: b ≤ 1) (H: q ≤ r) : b + q*2 ≤ 1 + r * 2 :=
-  calc
-    b + q*2 ≤ 1 + q * 2 : by apply nat.add_le_add_right Hb
-        ... ≤ 1 + r * 2 : by apply nat.add_le_add_left (nat.mul_le_mul_right 2 H)
-
-lemma foldr_product_bound {y: ℕ} {xs : list bool} :
-    list.foldr (λ (a : bool) (b : ℕ), b * 2 + bit_to_nat a) y xs ≤ list.foldr (λ (a : bool) (b : ℕ), b * 2 + 1) y xs :=
-  begin
-    induction xs with b,
-    case list.nil
-    { refl },
-    case list.cons
-    { simp, apply lt_q_r, apply bounded_bit_to_nat,
-      simp at xs_ih,
-      apply xs_ih,
-    }
-  end
-
-lemma product_bound (y: ℕ) (xs : list bool) :
-                    list.foldl (λacc b, acc*2 + bit_to_nat b) y xs ≤ list.foldl (λacc _, acc*2 + 1) y xs :=
-  begin
-    rw [foldl_foldr, foldl_foldr],
-    apply foldr_product_bound
-  end
-
 section bitwise
-  variable {n : ℕ}
 
-  def bitvec_to_bits : Π{n:ℕ} (x: bitvec n), vector bool n
-    | 0     _ := nil
-    | (n+1) x := msb x :: @bitvec_to_bits n ⟨x.val / 2, by apply (div_2_lt x.property)⟩
+  -- A fixed width version of nat.bitwise
+  -- This applies `f` to each bit in tthe vectors.
+  def fin_bitwise (f: bool → bool → bool) : ℕ → ℕ → ℕ → ℕ
+    | 0 _  _:= 0
+    | (nat.succ w) x y :=
+      let xr := x.bodd_div2 in
+      let yr := y.bodd_div2 in
+      nat.bit (f xr.fst yr.fst) (fin_bitwise w xr.snd yr.snd)
 
-  def bits_to_bitvec (x: vector bool n) : bitvec n :=
-    ⟨ list.foldl (λacc b, acc*2 + bit_to_nat b) 0 (to_list x),
-      begin
-        have bounded := product_bound 0 (to_list x),
-        rw [product_eq 0 (to_list x) x.property, nat.mul_comm (2^n) 0, zero_mul, zero_add] at bounded,
-        have bounded := lt_succ_of_le bounded,
-        have pos : 2^n > 0, { apply pos_pow_of_pos, apply succ_pos },
-        have succ_rw : succ (2 ^ n - 1) = succ (pred (2 ^ n)), { refl },
-        rw [succ_rw, succ_pred_eq_of_pos pos] at bounded,
-        exact bounded
-      end⟩
+  -- A theorem that nat.bit is less than a power of two, when the input
+  -- is.
+  --
+  -- The implicit parameters are chosen so that apply is useful here.
+  theorem bit_lt_pow2  {w:ℕ} {b : bool} {x : ℕ} (h : x < 2^w)
+  : nat.bit b x < 2^(w+1) :=
+  begin
+    -- Simplify 2^(w+1)
+    simp [nat.add_succ, nat.pow_succ, nat.mul_comm _ 2, eq.symm (nat.bit0_val _)],
+    -- Split on b and simplify bit
+    cases b; simp [bit],
+    case ff { apply nat.bit0_lt h, },
+    case tt { apply nat.bit1_lt_bit0 h, }
+  end
+
+  theorem bitwise_lt (f: bool → bool → bool) (w:ℕ)
+  : ∀(x y : ℕ),  fin_bitwise f w x y < 2^w :=
+  begin
+    induction w with w p,
+    { intros, dec_trivial_tac, },
+    intros,
+    unfold fin_bitwise,
+    apply bit_lt_pow2 (p _ _),
+  end
 
   @[reducible]
-  def bit_map₂ (f: bool → bool → bool) (x: bitvec n) (y: bitvec n) : bitvec n :=
-    bits_to_bitvec (map₂ f (bitvec_to_bits x) (bitvec_to_bits y))
+  def bitwise {w:ℕ} (f: bool → bool → bool) (x y : bitvec w) : bitvec w :=
+    ⟨fin_bitwise f w x.val y.val, bitwise_lt _ _ _ _⟩
 
-  def not (x: bitvec n) : bitvec n := ⟨2^n - x.val - 1,
+  @[reducible]
+  def not {w:ℕ} (x: bitvec w) : bitvec w := ⟨2^w - x.val - 1,
     begin
       have xval_pos : 0 < x.val + 1,
       { apply (succ_pos x.val) },
@@ -231,9 +226,9 @@ section bitwise
       apply (succ_pos 1)
     end⟩
 
-  def and : bitvec n → bitvec n → bitvec n := bit_map₂ band
-  def or  : bitvec n → bitvec n → bitvec n := bit_map₂ bor
-  def xor : bitvec n → bitvec n → bitvec n := bit_map₂ bxor
+  def and {w:ℕ} : bitvec w → bitvec w → bitvec w := bitwise band
+  def or  {w:ℕ} : bitvec w → bitvec w → bitvec w := bitwise bor
+  def xor {w:ℕ} : bitvec w → bitvec w → bitvec w := bitwise bxor
 
 end bitwise
 
